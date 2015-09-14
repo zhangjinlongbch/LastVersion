ea9053c612c61e64cf90d732afdebd77fe8657ec
BCH_ZhangJinlong <1050483798@qq.com>
2015-09-14 15:52:47 +0800

    添加dbhelper类~~~我真是醉了~~~~

---
+++ EssayBashBoard/EssayBashBoard/DBHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EssayBashBoard
+{
+    public class DBHelper
+    {
+        private SqlConnection con;
+        public DBHelper()
+        {
+            string ConStr = System.Configuration.ConfigurationManager.ConnectionStrings["SqlServerConnection"].ConnectionString;
+            con = new SqlConnection(ConStr);
+        }
+        public int SqlExcute(string CmdText)
+        {
+            var i = 0;
+            con.Open();
+            SqlCommand cmd = new SqlCommand(CmdText,con);
+            i = cmd.ExecuteNonQuery();
+            con.Close();
+            return i;
+        }
+        public int SqlReader(string CmdText)
+        {
+            var i = 0;
+            con.Open();
+            SqlCommand cmd = new SqlCommand(CmdText,con);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                i++;
+            }
+            con.Close();
+            return i;
+        }
+    }
+}
