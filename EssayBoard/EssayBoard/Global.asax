01b5d5a1da9bcae7c33fea9d5f19044c859d265e
jinhui <Proton97.mail@gmail.com>
2015-09-14 09:55:02 +0800

    update

---
+++ EssayBoard/EssayBoard/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EssayBoard.MvcApplication" Language="C#" %>
---
+++ EssayBoard/EssayBoard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EssayBoard.Startup))]
+namespace EssayBoard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
