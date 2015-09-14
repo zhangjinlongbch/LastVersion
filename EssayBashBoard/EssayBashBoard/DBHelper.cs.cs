36a9df5cd644d6236a56aa6f9f36cf70db003aa1
BCH_ZhangJinlong <1050483798@qq.com>
2015-09-14 11:41:32 +0800

    no message

---
+++ EssayBashBoard/EssayBashBoard/DBHelper.cs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EssayBashBoard
+{
+    public class DBHelper
+    {
+        private SqlConnection con;
+        public DBHelper()
+        {
+            string ConStr = System.Configuration.ConfigurationManager.ConnectionStrings["SqlServerConnection"].ConnectionString;
+            con = new SqlConnection(ConStr);
+        }
+        //执行SqlExcute方法，并返回受影响的行数
+        public int SqlExcute(string CommandText)
+        {
+            int n = 0;
+            con.Open();
+            SqlCommand cmd = new SqlCommand(CommandText,con);
+            n = cmd.ExecuteNonQuery();
+            con.Close();
+            return n;
+        }
+    }
+}
