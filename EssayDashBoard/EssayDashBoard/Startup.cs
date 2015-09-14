0848ab988a744d086ebe13cb10c393193d5d0e5b
jinhui <Proton97.mail@gmail.com>
2015-09-14 09:47:26 +0800

    update

---
+++ EssayDashBoard/EssayDashBoard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EssayDashBoard.Startup))]
+namespace EssayDashBoard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
